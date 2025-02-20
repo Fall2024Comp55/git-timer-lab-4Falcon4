95a2e4ca5f44b38d87f802715e6b5a83f6d5e9c9
4falcon4 <falcon@4falcon4.com>
2025-02-19 22:13:16 -0800

    I think if a pause would be used in the BallLauncher actionPerformed it would pause all the entire screen from updating including the balls on screen. Making it look like it frooze for a bit.

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -1,38 +1,55 @@
 import acm.graphics.*;
 import acm.program.*;
+
 import java.awt.*;
+import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
-import javax.swing.*;
+public class BallLauncher extends GraphicsProgram implements ActionListener {
+    public static final int PROGRAM_HEIGHT = 600;
+    public static final int PROGRAM_WIDTH = 800;
+    public static final int SIZE = 25;
+    public static final int MS = 50;
+    public static final int SPEED = 5;
+    private ArrayList<GOval> balls;
 
-public class BallLauncher extends GraphicsProgram{
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	public static final int SIZE = 25;
-	
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public void run() {
-		addMouseListeners();
-	}
-	
-	public void mousePressed(MouseEvent e) {
-		GOval ball = makeBall(SIZE/2, e.getY());
-		add(ball);
-	}
-	
-	public GOval makeBall(double x, double y) {
-		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
-		temp.setColor(Color.RED);
-		temp.setFilled(true);
-		return temp;
-	}
-	
-	public static void main(String[] args) {
-		new BallLauncher().start();
-	}
+    public void init() {
+        setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+        balls = new ArrayList<>();
+        requestFocus();
+    }
+
+    public void run() {
+        addMouseListeners();
+
+
+        Timer t = new Timer(MS, this);
+        t.start();
+    }
+
+    public void mousePressed(MouseEvent e) {
+        GOval ball = makeBall(SIZE / 2, e.getY());
+        balls.add(ball);
+        add(ball);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        balls.forEach(ball -> {
+			ball.move(SPEED, 0);
+		});
+
+
+    }
+
+    public GOval makeBall(double x, double y) {
+        GOval temp = new GOval(x - SIZE / 2, y - SIZE / 2, SIZE, SIZE);
+        temp.setColor(Color.RED);
+        temp.setFilled(true);
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        new BallLauncher().start();
+    }
 
 }
